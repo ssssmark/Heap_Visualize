084a923e097a190da0c9df91add3712c3805763d
ssssmark <111336914+ssssmark@users.noreply.github.com>
2023-09-04 10:17:08 +0800

    清理注释

--- src/App.js
+++ src/App.js
@@ -57,12 +57,6 @@ class App extends React.Component {
         let temp = A[i];
         A[i] = A[j];
         A[j] = temp;
-
-        /**
-         * tips:这里设置当前组件state.allValue的值是没问题的，肯定可以成功的，如果有视图中使用了allValue的值，那么视图也会更新。
-         * 1.下面视图中的canvas组件也传递了state.allValue是没有问题的
-         * 2.为何state.allValue没有触发canvas组件的渲染，请看canvas组件内的tips
-         */
         const root = this.buildBinaryTree(A);
         console.log("swap root = ", root);
             this.setState({
@@ -103,13 +97,7 @@ class App extends React.Component {
             j++; // 找到两个孩子中较大的一个，再与父节点比较
         }
         if (temp < Number(A[j].value)) {
-            /*A[i].isHighlight=true;
-            A[j].isHighlight=true;*/
             this.swap(A, i, j); // 如果父节点小于子节点:交换；否则跳出
-         /*   this.asyncSleep(2000).then(()=>{
-                A[i].isHighlight=false;
-                A[j].isHighlight=false;
-            })*/
             i = j; // 交换后，temp 的下标变为 j
         } else {
             return;
@@ -124,11 +112,8 @@ class App extends React.Component {
         for (let i = Math.floor(A.length / 2 - 1); i >= 0; i--) {
             this.shiftDown(A, i, A.length);
         }
-            window.alert("大根堆构造完成")
-
         /**
-         * issus:
-         * 1.这里导致的为何你延迟了2s，但是视图没有按照你预期的2s一更新
+         * 1.这里导致的为何延迟了2s，但是视图没有按照预期的2s一更新
          * 2.首先这不是bug，这是react的渲染机制导致
          * 3.你这里循环去setState，react会默认将其合并为一次进行render渲染，所以你的延迟2s后的setState实际上被react合并成了一次进行渲染
          * 4.这你就理解为啥swap内打印符合预期，但是渲染不符合预期
@@ -140,7 +125,6 @@ class App extends React.Component {
         //   this.shiftDown(A, 0, i, times); // 从根节点开始调整
         // }
         /**
-         * issus:这个函数为啥能实现延迟2s后再执行，而不被react合并render
          * 1.对于react内部机制而言，他没法去分析异步代码的逻辑，所以此函数它只会分析到下面注释行的逻辑
          * 2.分析到了的setState就会被合并，显然，你只有this.swap有setState，所以自然就被render渲染了
          * 3.然后我们判断是否循环完整个数组，如果没有，那么我们就延迟2s后再次调用此函数，此时，react内部机制就没法分析到这个延迟2s后的逻辑了
@@ -165,19 +149,10 @@ class App extends React.Component {
             });
         }
     }
-
-    /**tips
-     * 异步延迟函数
-     * @param {number} delay 延迟毫秒数
-     * @example await syncSleep(1024) // 延迟1秒，使用时所处的函数块需要加上async关键字
-     */
     asyncSleep(delay) {
         return new Promise((resolve) => setTimeout(resolve, delay));
     }
-    /**tips
-     * 同步延迟函数-不建议这种方式
-     * @param {number} delay 延迟毫秒数
-     */
+
     syncSleep(delay) {
         let start = new Date().getTime();
         while (new Date().getTime() - start < delay) {
--- src/components/canvas.js
+++ src/components/canvas.js
@@ -1,15 +1,6 @@
 import React from "react";
 import TreeNode from "./treenode";
 import "./canvas.css";
-/**
- * tips：当前组件没有接收/视图中也没有使用上面组件传递下面的allValue，那么自然不会触发渲染，这是七一
- * 看下面的getDerivedStateFromProps生命周期内，你只比较props.root变化了就允许组件响应变化，其他的都直接返回null部响应渲染了，这是其二
- * 你当前组件也没有使用allValue，如果不需要使用，父组件最好也不要传递，额外增加组件性能损耗
- * 当前组件目前只使用了props.root到视图中，所以只有props.root的改变会触发组件重新渲染
- * 特别注意：你传递了allValue，但是不用，组件的逻辑可能会重新生成，但是，react内部通过fiber机制会去对比前后生成的实际视图进行对比，在逻辑上更新并不等于在视图上更新。
- * 举个例子：你在组件内监听了allValue的更改并且console.log出来了，你父组件更改allValue，当前组件也成功打印，但是视图本身没有更新哦，因为你allValue并没有在
- * 视图中进行绑定使用。
- */
 export default class Canvas extends React.Component {
     constructor(props) {
         super(props);
@@ -55,12 +46,6 @@ export default class Canvas extends React.Component {
                 const node = queue.shift();
                 x = gapX * (i + 1);
                 y = (depth + 1) * gapY;
-                /***
-                 * tips：要使此处的TreeNode节点重新渲染，当前情况下一般会渲染，因为你是通过函数返回的节点构造，相当于每次都是新节点。
-                 * 但是要注意因为你此处是使用了循环，且自己控制了key，所以有些情况下不会渲染甚至堵塞，例如两个节点的值都是8，那么key也是相同的，就会报错冲突
-                 * 想要节点每次数据变化都渲染：只需要在当前循环内随机生成一个随机字符当做key即可，这里我已经将对应代码注入了
-                 *  const nowKey = this.randomString(16)
-                 */
                 const nowKey = this.randomString(16)
                 tree.push(
                     <TreeNode
@@ -99,23 +84,3 @@ export default class Canvas extends React.Component {
     }
 }
 
-
-
-// export default class ComponentName extends React.Component {
-//     // 组件的状态直接写这样，不通过函数进行声明赋值，提效，使用也是通过this.state使用
-//     state = {}
-//     constructor(){
-//         super(props);
-//         // 这里用来绑定函数this指向等副作用纠正操作，不需要去设置this.state
-//     }
-//     // ...生命周期函数
-//     // ...自定义函数
-
-
-
-//     render(){
-//         const {}=this.props; // render内部使用props最好解构出来，不要在视图中直接this.props.xxx
-//         const {}=this.state; // render内部使用state最好解构出来，不要在视图中直接this.state.xxx
-//         return <></>
-//     }
-// }
